26b6f7b70baea554e62efcca1d34a94912b630d0
Joy_Cydeo <fsata86@gmail.com>
2023-03-09 11:56:58 -0500

    User Story is done

---
+++ src/test/java/com/library/step_definitions/US61_StepDefs.java
@@ -0,0 +1,49 @@
+package com.library.step_definitions;
+
+import com.library.pages.UsersPage_JI;
+import com.library.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.Collections;
+import java.util.List;
+
+public class US61_StepDefs {
+
+    UsersPage_JI usersPage_ji = new UsersPage_JI();
+
+    @Given("I am on the login page")
+    public void ı_am_on_the_login_page() {
+        usersPage_ji.webPage();
+
+
+    }
+
+    @Given("I login as a librarian")
+    public void ı_login_as_a_librarian() {
+        usersPage_ji.login("librarian");
+    }
+
+    @When("I click on Users link")
+    public void ı_click_on_link() {
+        usersPage_ji.usersbutton.click();
+
+    }
+
+    @Then("Each user id should be unique")
+    public void each_user_id_should_be_unique() {
+
+            List<WebElement> elements = Driver.getDriver().findElements(By.xpath("//div[@class='table-scrollable']//tbody//tr//td[2]"));
+            for (WebElement eachElement : elements) {
+                int frequency = Collections.frequency(elements, eachElement);
+                Assert.assertEquals(frequency, 1);
+            }
+
+        }
+
+    }
+
---
+++ src/test/resources/features/US61_ haveAUniqueID.feature
@@ -0,0 +1,9 @@
+
+
+Feature: Check Uniq ID
+
+  Scenario: unique user ids
+    Given I am on the login page
+    And I login as a librarian
+    When I click on Users link
+    Then Each user id should be unique
